9ef07065b277098ea878346887dc2336bc4e9c67
risingone <anuragdhingra101@gmail.com>
2017-11-05 07:50:07 +0530

    Finish static pages

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,21 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+
+	test "should get home" do 
+		get static_pages_home_url 
+		assert_response :success 
+	end
+
+	test "should get help" do
+		get static_pages_help_url
+		assert_response :success 
+	end
+
+	test "should get about" do
+		get static_pages_about_url
+		assert_response :success
+	end
+	
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
